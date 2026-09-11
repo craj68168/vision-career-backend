2f64ce257fda4e31a67a1de174fbf575baf64b21
Jyoti Shah <jyotishahqwerty@gmail.com>
2026-09-11 12:11:45 +0545

    feat:initial comments

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use("/api/profile", profileRoutes);
-
+console.log
 // ---------- MongoDB connection ----------
 mongoose
   .connect(process.env.MONGO_URI)
